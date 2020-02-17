d77590632602d2f0dcedd5e4e2f461addb8c8a09
Cristian Araujo <araujocristian@live.com>
2020-02-16 22:08:07 -0300

    estilizando list e board

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import GlobalStyle from "./styles/global";
 
 import Header from "./components/Header";
 import Board from "./components/Board";
-import List from "./components/List";
 
 function App() {
   return (
     <>
       <Header />
       <Board />
-      <List />
       <GlobalStyle />
     </>
   );
